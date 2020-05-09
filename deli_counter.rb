28d426635466e3fe56821cdabeb6dde5a9e9164f
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-09 15:54:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,6 @@
 
 def line (katz_deli)
+    line_is_currently = "The Line is currently:"
     line_is_currently = katz_deli.each.with_index{|name, index| puts "The line is currently: #{index += 1}. #{name}"}
     
     if katz_deli == []
