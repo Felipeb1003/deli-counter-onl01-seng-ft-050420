0059ac050c8fbaac7e3be91de1a756389eb70f6c
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-09 16:36:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,10 +20,12 @@ end
 
 def now_serving(katz_deli)
   
+   
   if  katz_deli == []
     puts "There is nobody waiting to be served!"
     
   else
+    puts "Currently serving #{katz_deli}"
     katz_deli.shift
     
   end
