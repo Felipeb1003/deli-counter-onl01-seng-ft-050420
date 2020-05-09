b94f6fdab8cf225a932eb07e74c4d1263c220ded
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-09 15:56:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,14 @@
 
 def line (katz_deli)
     line_is_currently = "The Line is currently:"
-    new string = katz_deli.each.with_index{|name, index| puts "The line is currently: #{index += 1}. #{name}"}
+    new_string = katz_deli.each.with_index{|name, index| puts "The line is currently: #{index += 1}. #{name}"}
     
     if katz_deli == []
       puts "The line is currently empty."
     else
      loop do |array|
+       line_is_currently << new_string
+     end
     end
 end
     
