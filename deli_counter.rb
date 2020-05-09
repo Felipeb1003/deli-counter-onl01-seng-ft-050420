b49b262e3c9b277381cf305c0c4ebb7bcbdd4c64
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-09 16:14:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,8 @@ def line (katz_deli)
     
 end
     
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    size_array = katz_deli.length
+    puts "Welcome, #{katz_deli}. You are number #{size_array} in line"
+end
